4cdeee2a6ed026906ec29c1834da012519102262
Hemang Maan <hemangmaan@gmail.com>
2021-11-26 11:25:08 +0530

    API key

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,4 +1,4 @@
-const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const API_KEY = 'api_key=7f5719644de5f05dfc066ec0484d202c';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
